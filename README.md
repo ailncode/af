26f8a425f8ca1ba36f745bd0ab3c8c190aacbc75
ailncode <ailncode@qq.com>
2018-12-22 18:11:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,119 @@
+What is AF
+-------------------------------------------------------------------------------------------------
+*The name of AF comes from air refueling.*
+*AF is a package of Go.*
+*You can use the AF write a graceful reload HTTP server easily.*
+**AF only support the Linux system now.**
+
+Featuer
+-------------------------------------------------------------------------------------------------
+
+* graceful reload
+* graceful stop
+* custom signal handler
+
+
+Install
+-------------------------------------------------------------------------------------------------
+
+```	go
+go get -u github.com/ailncode/af
+```
+
+Usage
+-------------------------------------------------------------------------------------------------
+
+1. Import the AF
+
+```go
+import github.com/ailncode/af
+```
+
+2. Simple use
+
+```go
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"github.com/ailncode/af"
+)
+
+func main(){
+    http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request) {
+		fmt.Fprintln(w, "ok")
+	})
+    refuel := af.Default()
+    //refuel.Addr = ":8080"
+    //:8080 is the default Addr
+    //refuel.Handler = http.DefaultServeMux
+    //http.DefaultServeMux is the default HTTP handler
+    //refuel.ShutdownTimeOut = time.Second * 10
+    //time.Second * 10 is the default ShutdownTimeOut
+    refuel.Run()
+    //You can check error in here.
+}
+```
+3.Reload & Stop your server
+
+```shell
+#Reload
+kill -USR2 <pid>
+#Stop
+kill -INT <pid> #or kill -TERM <pid>
+```
+
+4. Use your custom Listen Address Handler ShutdownTimeOut
+
+```go
+package main
+
+import(
+	"net/http"
+    "time"
+	"github.com/ailncode/af"
+    "github.com/gin-gonic/gin"
+)
+
+func main(){
+    //Use handler like *gin.Engine
+    router :=gin.Default()
+	router.GET("/", func(context *gin.Context) {
+		context.String(http.StatusOK,"ok")
+	})
+    refuel := af.Default()
+    refuel.Handler = router
+    //You can use the below code also.
+    //af := af.NewWithHandler(r)
+    refuel.Addr = ":80"
+    refuel.ShutdownTimeOut = time.Second * 10
+    refuel.Run()
+}
+```
+
+5. Use your custom system signal handler
+
+```go
+package main
+
+import (
+	"af"
+	"syscall"
+	"time"
+)
+
+func main() {
+	refuel := af.Default()
+	refuel.ShutdownTimeOut = time.Second * 10
+	refuel.HandleSignal(func(r *af.AF) {
+		r.Stop()
+		//You can check error in here
+	}, syscall.SIGINT, syscall.SIGTERM)
+	refuel.HandleSignal(func(r *af.AF) {
+		r.Reload()
+		//You can check error in here
+	})
+	refuel.Run()
+}
+```
---
+++ af.go
@@ -0,0 +1,173 @@
+// Copyright 2018 ailn(ailnindex@qq.com). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//Package af provides easy to use graceful restart a http server
+
+package af
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"reflect"
+	"syscall"
+	"time"
+)
+
+type AF struct {
+	Addr             string
+	Handler          http.Handler
+	ShutdownTimeOut  time.Duration
+	listener         net.Listener
+	server           *http.Server
+	signalSlice      []os.Signal
+	signalHandlerMap map[os.Signal]func(*AF)
+	graceful bool
+}
+
+//Get default AF
+func Default() *AF {
+	return &AF{
+		Addr:            ":8080",
+		ShutdownTimeOut: time.Second * 10,
+	}
+}
+
+//Get AF with a http handler
+func NewWithHandler(handler http.Handler) *AF {
+	return &AF{
+		Addr:            ":8080",
+		Handler:		handler,
+		ShutdownTimeOut: time.Second * 10,
+	}
+}
+
+//Get AF with a http server
+func NewWithServer(server *http.Server) *AF {
+	return &AF{
+		server:          server,
+		Addr:            server.Addr,
+		Handler:         server.Handler,
+		ShutdownTimeOut: time.Second * 10,
+	}
+}
+
+//init AF
+func (af *AF) init() error {
+	if af.server == nil {
+		if af.Handler == nil{
+			af.Handler = http.DefaultServeMux
+		}
+		af.server = &http.Server{
+			Addr:           af.Addr,
+			Handler:        af.Handler,
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+		}
+	}
+	var err error
+	if af.graceful{
+		f := os.NewFile(3, "")
+		af.listener , err = net.FileListener(f)
+	}else{
+		af.listener, err = net.Listen("tcp", af.server.Addr)
+	}
+	return err
+}
+
+//Give some default signal handler if AF' signalHandlerMap is nil
+func(af *AF)defaultSignalHandle(){
+	af.HandleSignal(func(af *AF){
+		log.Println(fmt.Sprintf("AF server is shutdown pid:%d err:%v",os.Getpid(),af.Stop()))
+	},syscall.SIGINT,syscall.SIGTERM)
+	af.HandleSignal(func(af *AF){
+		log.Println(fmt.Sprintf("AF server is reload pid:%d err:%v",os.Getpid(),af.Reload()))
+	},syscall.SIGUSR2)
+}
+
+//signal handle
+func (af *AF) signalHandle() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, af.signalSlice...)
+	for {
+		select {
+		case s := <-signalChan:
+			if f, ok := af.signalHandlerMap[s]; ok {
+				f(af)
+			}
+			break
+		}
+	}
+}
+
+//Give a handler for signals
+func (af *AF) HandleSignal(handler func(*AF), signals ...os.Signal) {
+	if af.signalHandlerMap == nil {
+		af.signalHandlerMap = make(map[os.Signal]func(*AF))
+	}
+	for _, s := range signals {
+		if _, ok := af.signalHandlerMap[s]; !ok {
+			af.signalSlice = append(af.signalSlice, s)
+		}
+		af.signalHandlerMap[s] = handler
+	}
+}
+
+//Run the AF
+func (af *AF) Run() error {
+	flag.BoolVar(&af.graceful,"graceful", false, "listen on fd open 3 (internal use only)")
+	flag.Parse()
+	var err error
+	if err = af.init(); err != nil {
+		return err
+	}
+	go func(af *AF) {
+		if af.signalHandlerMap == nil{
+			af.defaultSignalHandle()
+		}
+		af.signalHandle()
+	}(af)
+	log.Println(fmt.Sprintf("AF server is run at pid:%d",os.Getpid()))
+	err = af.server.Serve(af.listener)
+	return err
+}
+
+//Stop the AF
+func (af *AF) Stop() error {
+	ctx, _ := context.WithTimeout(context.Background(), af.ShutdownTimeOut)
+	return af.server.Shutdown(ctx)
+}
+
+//Reload the AF
+func (af *AF) Reload() error {
+	listener, ok := af.listener.(*net.TCPListener)
+	if !ok {
+		return errors.New("AF.listener type must be *net.TCPListener,but got type " + reflect.TypeOf(af.listener).Name())
+	}
+	file, err := listener.File()
+	if err != nil {
+		return err
+	}
+	args := os.Args
+	if !af.graceful{
+		args = append(args,"--graceful")
+	}
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.ExtraFiles = []*os.File{file}
+	err = cmd.Start()
+	if err==nil{
+		return af.Stop()
+	}
+	return err
+}
