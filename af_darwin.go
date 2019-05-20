e51430b4929cf58766f0a276fa17f0ed5dbc53f4
ailn <ailnindex@qq.com>
2019-05-20 09:36:19 +0800

    support os darwin

---
+++ af_darwin.go
@@ -0,0 +1,24 @@
+// Copyright 2018 ailn(ailnindex@qq.com). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//Package af provides easy to use graceful restart a http server
+
+package af
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"syscall"
+)
+
+//Give some default signal handler if AF' signalHandlerMap is nil
+func (af *AF) defaultSignalHandle() {
+	af.HandleSignal(func(af *AF) {
+		log.Println(fmt.Sprintf("AF server is shutdown pid:%d err:%v", os.Getpid(), af.Stop()))
+	}, syscall.SIGINT, syscall.SIGTERM)
+	af.HandleSignal(func(af *AF) {
+		log.Println(fmt.Sprintf("AF server is reload pid:%d err:%v", os.Getpid(), af.Reload()))
+	}, syscall.SIGUSR2)
+}
